8632e32756d86730a36d62524ab39c8cbe0a5f61
Lev Yuvenskiy <yuvenskiylev@mail.ru>
2024-10-20 22:22:13 +0300

    Refactor

--- app/src/core/api/index.ts
+++ app/src/core/api/index.ts
@@ -1,6 +1,5 @@
 import { IRequestOptions } from "./typing.ts";
 
-/* export const BASE_URL = "http://localhost:8080"; */
 export const BASE_URL = "/api";
 
 export const sendRequest = async (options: IRequestOptions) => {
@@ -9,20 +8,15 @@ export const sendRequest = async (options: IRequestOptions) => {
         path,
         headers = {},
         params = undefined,
-        data /* , isAuth = false */,
+        data
     } = options;
 
-    /* const url = new URL(`${BASE_URL}${path}`); */
     let url = BASE_URL + path;
 
-    /*
-    const accessToken = await getCurrentAccessToken();
-    console.log("CurrentAccessToken", accessToken); */
     const requestOptions: RequestInit = {
         method,
         headers: {
             "Content-Type": "application/json",
-            //Authorization: !isAuth && accessToken ? /* "Bearer " + */ accessToken : "",
             ...headers,
         },
         body: data ? JSON.stringify(data) : undefined,
@@ -30,12 +24,11 @@ export const sendRequest = async (options: IRequestOptions) => {
 
     if (params) {
         Object.keys(params).forEach((key) => {
-            /* url.searchParams.append(key, params[key]); */
             url = url + "?" + key + "=" + params[key];
         });
     }
 
-    const response = await fetch(url/* .toString() */, requestOptions);
+    const response = await fetch(url, requestOptions);
 
     if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
