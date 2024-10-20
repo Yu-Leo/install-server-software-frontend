55be5f151c7067d84586ba4cd7e004a38d62af57
Lev Yuvenskiy <yuvenskiylev@mail.ru>
2024-10-20 18:51:49 +0300

    Fix routing

--- app/src/Routes.tsx
+++ app/src/Routes.tsx
@@ -1,6 +1,5 @@
 import {RouteObject, useRoutes} from "react-router-dom";
 import {IGlobalProps} from "./App.typing";
-import {PrivatePages} from "./components/PricatePages";
 import {MainPage} from "./pages/MainPage";
 import {SoftwareCatalogPage} from "./pages/SoftwareCatalogPage";
 import {SoftwarePage} from "./pages/SoftwarePage";
@@ -9,32 +8,22 @@ import {InstallSoftwareRequestPage} from "./pages/InstallSoftwareRequestPage";
 export const AppRoutes = (props: IGlobalProps) => {
     const routes: RouteObject[] = [
         {
-            path: "/",
-            element: <PrivatePages {...props} />,
-            children: [
-                {
-                    path: "",
-                    element: <MainPage {...props} />,
-                },
-                {
-                    path: "software_catalog",
-                    element: <SoftwareCatalogPage {...props} />,
-                },
-                {
-                    path: "software/:id",
-                    element: <SoftwarePage/>,
-                },
-
-                {
-                    path: "install_software_request/:id",
-                    element: <InstallSoftwareRequestPage {...props}/>,
-                },
-            ],
+            path: "",
+            element: <MainPage {...props} />,
         },
-        // {
-        //     path: "*",
-        //     element: <NotFoundPage />,
-        // },
+        {
+            path: "software_catalog",
+            element: <SoftwareCatalogPage {...props} />,
+        },
+        {
+            path: "software/:id",
+            element: <SoftwarePage/>,
+        },
+
+        {
+            path: "install_software_request/:id",
+            element: <InstallSoftwareRequestPage {...props}/>,
+        },
     ];
 
     const routeResult = useRoutes(routes);
