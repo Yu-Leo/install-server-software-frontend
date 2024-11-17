194e57fb5be7cf871de47d070b2eee3c8642ace3
Lev Yuvenskiy <yuvenskiylev@mail.ru>
2024-11-17 17:50:40 +0300

    Update ISR Table

--- app/src/components/ISRTable/index.tsx
+++ app/src/components/ISRTable/index.tsx
@@ -1,39 +1,69 @@
 import {FC} from "react";
 import {IISRTableProps} from "./typing.tsx";
-import {Table} from "react-bootstrap";
+import {Card, Col, Row} from "react-bootstrap";
 import {Link} from "react-router-dom";
 
 
 export const ISRTable: FC<IISRTableProps> = (props: IISRTableProps) => {
     return (
-        <Table striped bordered hover>
-            <thead>
-            <tr>
-                <th>№</th>
-                <th>Статус</th>
-                <th>Дата создания</th>
-                <th>Дата оформления</th>
-                <th>Дата завершения</th>
-            </tr>
-            </thead>
-            <tbody>
+        <div>
+            <Card className="mb-2">
+                <Card.Body className="py-2 px-3">
+                    <Row className="d-flex align-items-center">
+                        <Col xs={12} sm={3}>
+                            <Card.Text><strong>№</strong></Card.Text>
+                        </Col>
+                        <Col xs={12} sm={3}>
+                            <Card.Text><strong>Статус</strong></Card.Text>
+                        </Col>
+                        <Col xs={12} sm={2}>
+                            <Card.Text><strong>Дата создания</strong></Card.Text>
+                        </Col>
+                        <Col xs={12} sm={2}>
+                            <Card.Text><strong>Дата оформления</strong></Card.Text>
+                        </Col>
+                        <Col xs={12} sm={2}>
+                            <Card.Text><strong>Дата завершения</strong></Card.Text>
+                        </Col>
+                    </Row>
+                </Card.Body>
+            </Card>
+
             {props.rows.map((row) => (
-                <tr key={row.number}>
-                    <td>
-                        <Link
-                            to={"/install_software_request/" + row.number}
-                            className="text-black"
-                        >
-                            {row.number}
-                        </Link>
-                    </td>
-                    <td>{row.status}</td>
-                    <td>{row.creationDate}</td>
-                    <td>{row.registrationDate}</td>
-                    <td>{row.completionDate}</td>
-                </tr>
+                <Card key={row.number} className="mb-2">
+                    <Card.Body className="py-2 px-3">
+                        <Row className="d-flex align-items-center">
+                            <Col xs={12} sm={3}>
+                                <Card.Text>
+                                    <Link to={"/install_software_request/" + row.number} className="text-black">
+                                        {row.number}
+                                    </Link>
+                                </Card.Text>
+                            </Col>
+                            <Col xs={12} sm={3}>
+                                <Card.Text>
+                                    {row.status}
+                                </Card.Text>
+                            </Col>
+                            <Col xs={12} sm={2}>
+                                <Card.Text>
+                                    {row.creationDate}
+                                </Card.Text>
+                            </Col>
+                            <Col xs={12} sm={2}>
+                                <Card.Text>
+                                    {row.registrationDate}
+                                </Card.Text>
+                            </Col>
+                            <Col xs={12} sm={2}>
+                                <Card.Text>
+                                    {row.completionDate}
+                                </Card.Text>
+                            </Col>
+                        </Row>
+                    </Card.Body>
+                </Card>
             ))}
-            </tbody>
-        </Table>
+        </div>
     );
 };
