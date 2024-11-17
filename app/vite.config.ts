4e244325d1af04f4bc6496edaab459854e5a39b6
Lev Yuvenskiy <yuvenskiylev@mail.ru>
2024-11-17 18:54:07 +0300

    Fixes for run in local network

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -31,6 +31,7 @@ export default defineConfig({
     ],
     base: "/install-server-software-frontend/",
     server: {
+        host: "0.0.0.0",
         port: 3000,
         proxy: {
             "/api": {
